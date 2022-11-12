10ac7cba1f82a1c1400460a08e47cff14c4de2fb
juanmi72 <juanmifer@gmail.com>
2022-11-13 00:57:03 +0100

    lesson 288

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,6 +8,39 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
+// https://forkify-api.herokuapp.com/v2  // API
 
 ///////////////////////////////////////
+
+console.log('TESt');
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcae1'
+      //'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+    console.log(res, data.data.recipe);
+
+    // En la API tenemos una propiedad 'ok' que si es false es porque se ha producido un error y nos vale para generar un error nosotros.
+    if (!res.ok) throw Error(`${data.message} ${res.status}`);
+
+    // reformateamos los datos recibidos
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
